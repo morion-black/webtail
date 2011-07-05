6688de587151898c09d95b82c6aa9f6e0b94d262
Morion Black <morion.estariol@gmail.com>
2011-07-05 16:44:09 +0300

    small fixes

--- webtail-client/webtail-client.rb
+++ webtail-client/webtail-client.rb
@@ -1,28 +1,33 @@
 #!/usr/bin/env ruby
-require 'net/http'
+require "net/http"
 require "uri"
 require "yaml"
 require "rubygems"
 require "file/tail"
 
-Config = YAML.load_file 'config.yml'
+puts "Configfile (config.yml) not found" unless File.readable?('config.yml')
+
+begin
+	CONFIG = YAML.load_file('config.yml')
+rescue Exception => errmsg
+	puts "Configfile format error: #{errmsg}"
+end
 
 def sendLog(data)
-    uri = (URI.parse("http://#{Config['Connect']['server']}:#{Config['Connect']['port']}/logs/#{Config['Connect']['hostname']}"))
+    uri = (URI.parse("http://#{CONFIG['Connect']['server']}:#{CONFIG['Connect']['port']}/logs/#{CONFIG['Connect']['hostname']}"))
     
     puts "Sending PUT #{uri.request_uri} to #{uri.host}:#{uri.port}"
     Net::HTTP.start(uri.host, uri.port) do |http|
         headers = {'Content-Type' => 'text/plain; charset=utf-8'}
-        response = http.send_request('PUT', uri.request_uri, data,
-headers)
+        response = http.send_request('PUT', uri.request_uri, data, headers)
     puts "Response #{response.code} #{response.message}: #{response.body}"
     end
 end
 
 
 threads = []
-Config['Logs']['files'].each { |file|
-    threads << Thread.new(file) do
+CONFIG['Logs']['files'].each { |file|
+    threads << Thread.new do
         File::Tail::Logfile.open(file, :backward => 10) do |log|
             log.tail { |line|
                 puts file
